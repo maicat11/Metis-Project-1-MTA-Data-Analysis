adf9f7c9ed3be4eb8e19a68a1cacf7cc1f22da23
j-carson <44308120+j-carson@users.noreply.github.com>
2019-01-09 15:44:55 -0800

    Change weekday to b_weekday, add pickle option

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,21 +1,31 @@
 import pandas as pd
 import numpy as np
 
-def read_mta_file(path):
+def read_mta_file(path, pickle_ok=True):
     """Read the raw MTA turnstyle data and return a dataframe cleaned up as follows -
        * Dates between 04/01/2018 and 06/22/2018
        * Column names stripped
        * Column Date and Time combined into one column called date_time and 
          of the proper Numpy datetime type
        * Turnstile ID created from Unit + SCP
-       * New column 'weekday' indicates whether data is from a weekday (monday-friday)
+       * New column 'b_weekday' boolean indicates whether data is from a weekday (monday-friday)
     """
+    if pickle_ok:
+        try:
+            spring = pd.read_pickle("spring.pickle")
+            return spring
+        except:
+            pass
+        
+        
     raw_data = pd.read_csv(path)
     
     # trim by date first so that it's easier to hack on
     our_rows = raw_data[(raw_data.Date >= '04/01/2018') & 
                      (raw_data.Date < '06/22/2018')]
     spring = our_rows.copy()
+    
+    raw_data = 0 # to free memory?
     
     assert(spring.shape[0] == 2307938)
     
@@ -26,7 +36,9 @@ def read_mta_file(path):
     spring['ts_id'] = spring.Unit + '_' + spring.SCP
         
     spring['date_time'] = pd.to_datetime(spring.Date + ' ' + spring.Time, format="%m/%d/%Y %X")
-    spring['weekday'] = spring.date_time.dt.weekday < 5
+    spring['bweekday'] = spring.date_time.dt.weekday < 5
     
     spring = spring.drop(columns=['Date', 'Time', 'Division', 'Line Name'])    
+    spring.to_pickle("spring.pickle")
+    
     return spring
